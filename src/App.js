a3817c628ddb5e9ec34d451913c9bd95a2314c73
Debriks <deborah.robbiano@gmail.com>
2022-09-04 00:15:19 +0200

    Added credits

--- src/App.js
+++ src/App.js
@@ -6,10 +6,32 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App d-flex justify-content-center flex-column align-items-center">
+    <div className="App d-flex justify-content-center flex-column">
       <Header />
       <Search />
       <Results />
+      <small>
+        This project was coded by{" "}
+        <a href="https://iamagarden.be/" target="_blank" rel="noreferrer">
+          Deborah Robbiano
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/debriks/react-dictionary-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://funny-smakager-a3614b.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </small>
     </div>
   );
 }
